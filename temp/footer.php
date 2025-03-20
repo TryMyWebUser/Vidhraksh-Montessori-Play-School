2d9b7891c4f349034f06fcf4dc0eafd963333615
TryMyWebsites <trymywebsites@gmail.com>
2025-03-20 14:29:41 +0530

    `Changed "Design and Development By" to "Designed and Developed By" in footer.php`

--- temp/footer.php
+++ temp/footer.php
@@ -57,7 +57,7 @@
                 </div>
                 <div class="pt-[75px] overflow-x-hidden">
                     <div class="flex lg:flex-row flex-col justify-between lg:items-center pt-7.5 pb-8 border-t border-t-white border-opacity-20">
-                        <p class="wow fadeInLeft" data-wow-delay=".3s">© Design and Development By <a href="https://trymywebsites.com/">Trymywebsites</a></p>
+                        <p class="wow fadeInLeft" data-wow-delay=".3s">Designed and Developed By <a href="https://trymywebsites.com/">Trymywebsites</a></p>
                     </div>
                 </div>
             </div>
